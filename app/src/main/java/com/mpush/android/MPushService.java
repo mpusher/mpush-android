1c0d4e4535fa7668e9b1ba8049f8e20267aaf4ab
夜色 <yanxx@shinemo.com>
2016-09-23 14:13:53 +0800

    增加服务保活

--- app/src/main/java/com/mpush/android/MPushService.java
+++ app/src/main/java/com/mpush/android/MPushService.java
@@ -1,12 +1,34 @@
+/*
+ * (C) Copyright 2015-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     ohun@live.cn (夜色)
+ */
+
+
 package com.mpush.android;
 
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
-import com.mpush.android.BuildConfig;
 import com.mpush.api.Client;
 import com.mpush.api.ClientListener;
 
@@ -23,6 +45,7 @@ public final class MPushService extends Service implements ClientListener {
     public static final String EXTRA_PUSH_MESSAGE_ID = "push_message_id";
     public static final String EXTRA_USER_ID = "user_id";
     public static final String EXTRA_DEVICE_ID = "device_id";
+    private int SERVICE_START_DELAYED = 5;
 
     @Nullable
     @Override
@@ -31,8 +54,13 @@ public final class MPushService extends Service implements ClientListener {
     }
 
     @Override
+    public void onCreate() {
+        super.onCreate();
+        cancelAutoStartService(this);
+    }
+
+    @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        int ret = super.onStartCommand(intent, flags, startId);
         if (!MPush.I.hasStarted()) {
             MPush.I.checkInit(this).create(this);
         }
@@ -40,18 +68,48 @@ public final class MPushService extends Service implements ClientListener {
             if (MPushReceiver.hasNetwork(this)) {
                 MPush.I.client.start();
             }
-            return START_REDELIVER_INTENT;
+            MPushFakeService.startForeground(this);
+            flags = START_STICKY;
+            SERVICE_START_DELAYED = 5;
+            return super.onStartCommand(intent, flags, startId);
         } else {
+            int ret = super.onStartCommand(intent, flags, startId);
             stopSelf();
+            SERVICE_START_DELAYED += SERVICE_START_DELAYED;
             return ret;
         }
     }
+
+    /**
+     * service停掉后自动启动应用
+     *
+     * @param context
+     * @param delayed 延后启动的时间，单位为秒
+     */
+    private static void startServiceAfterClosed(Context context, int delayed) {
+        AlarmManager alarm = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarm.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + delayed * 1000, getOperation(context));
+    }
+
+    public static void cancelAutoStartService(Context context) {
+        AlarmManager alarm = (AlarmManager) context
+                .getSystemService(Context.ALARM_SERVICE);
+        alarm.cancel(getOperation(context));
+    }
+
+    private static PendingIntent getOperation(Context context) {
+        Intent intent = new Intent(context, MPushService.class);
+        PendingIntent operation = PendingIntent.getService(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        return operation;
+    }
+
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         MPushReceiver.cancelAlarm(this);
         MPush.I.destroy();
+        startServiceAfterClosed(this, SERVICE_START_DELAYED);//5s后重启
     }
 
     @Override
---
+++ app/src/main/java/com/mpush/android/MpushFakeService.java
@@ -0,0 +1,61 @@
+/*
+ * (C) Copyright 2015-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     ohun@live.cn (夜色)
+ */
+
+
+package com.mpush.android;
+
+import android.app.Notification;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+
+/**
+ * 双Service提高进程优先级,降低被系统杀死机率
+ * <p>
+ * Created by yxx on 2016/2/15.
+ *
+ * @author ohun@live.cn
+ */
+public final class MPushFakeService extends Service {
+    public static final int NOTIFICATION_ID = 1001;
+
+    public static void startForeground(Service service) {
+        service.startService(new Intent(service, MPushFakeService.class));
+        service.startForeground(NOTIFICATION_ID, new Notification());
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        startForeground(NOTIFICATION_ID, new Notification());
+        stopSelf();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        stopForeground(true);
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
